6893f50ab8d4e6da776bdf40f953c244c728d922
Mufeng Wei <mufeng_wei@berkeley.edu>
2024-10-11 12:45:42 -0700

    C4 uploaded

---
+++ ChengXuSheJiJiChu/Chapter 4/Prime facorization.cpp
@@ -0,0 +1,74 @@
+﻿// Pass
+
+//分解n个大于1的正整数的质因数，并以因子从小到大排序，以等式格式输出。比如
+//
+//```
+//
+//12 = 2 * 2 * 3
+//
+//```
+//
+//输入描述
+//第1行为一个整数n，表示需要进行分解的质因数的数量。(1 <= n <= 100)
+//
+//第2行到第n + 1行  为需要进行分解的n个正整数x_i。(1 < x_i < 10 ^ 9)
+//
+//    输出描述
+//    共n行，每一行为分解质因数得到的等式，因子从小到大进行排列
+//
+//    示例1 :
+//输入：
+//```
+//2
+//12
+//36
+//```
+//
+//输出：
+//```
+//12 = 2 * 2 * 3
+//36 = 2 * 2 * 3 * 3
+//```
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Input
+    int n;
+
+    // First, input the size of the array
+    cin >> n;
+
+    // Declare the array with size `n`
+    int * arr = new int[n];
+
+    // Input array elements
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Factorization of each element
+    for (int i = 0; i < n; i++) {
+        int t = 2;
+        cout << arr[i] << '=';
+        while (arr[i] != 1) {
+            if (arr[i] % t == 0) {
+                cout << t;
+                arr[i] = arr[i] / t;
+                if (arr[i] != 1) {
+                    cout << '*';
+                }
+                else {
+                    cout << endl;
+                }
+            }
+            else {
+                t++;
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ ChengXuSheJiJiChu/Chapter 4/Second largest number.cpp
@@ -0,0 +1,70 @@
+﻿// Pass
+
+//数学里有一个函数叫做max(a, b)，为a和b中更大的那一个。
+//
+//现在，请完成函数max2，要求返回当前遇见过的所有数字中，第二大的数字。
+//
+//你只能使用全局变量a1和a2来保存信息，不能使用数组来存储。
+//
+//(允许再另外使用两个变量分别用于存储n和用于输入正整数)
+//
+//输入描述
+//第一行一个数字n，表示有n个正整数，2 <= n <= 100。
+//
+//第二行有n个正整数，分别是数列的n项，数列各项互不相等。
+//
+//输出描述
+//输出只有一行，一个整数，表示数列的第二大数字。
+//
+//示例1 :
+//输入：
+//10
+//
+//10 9 8 7 6 5 4 3 2 1
+//输出：
+//9
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    // Take input for the size of the array
+    cin >> n;
+
+    // Define the array after the size is known
+    int * arr = new int[n];
+
+    // Input the array elements
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Initialize max1 and max2
+    int max1 = arr[0], max2 = arr[1];
+
+    // Swap max1 and max2 if necessary
+    if (max1 < max2) {
+        int temp = max1;
+        max1 = max2;
+        max2 = temp;
+    }
+
+    // Find the two largest values
+    for (int i = 2; i < n; i++) {
+        if (arr[i] > max1) {
+            int temp = max1;
+            max1 = arr[i];
+            max2 = temp;
+        }
+        else if (arr[i] > max2) {
+            max2 = arr[i];
+        }
+    }
+    cout << max2 << endl;
+
+    delete[] arr;
+    return 0;
+}
